fcfba4e8ac5660ab7cbe615a3d7fd295b62faf7f
michaelhly <michaelhly@gmail.com>
2019-11-05 23:08:51 -0600

    Add bulma and eslint

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="columns">
+      <div className="column">1</div>
+      <div className="column">2</div>
+      <div className="column">3</div>
+      <div className="column">4</div>
+    </div>
+  );
+};
+
+export default App;
